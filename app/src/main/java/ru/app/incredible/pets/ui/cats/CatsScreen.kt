73b6043d6e4c521bc1481172307a5785d0e35ce4
Alexander Rovnov <asrovnov@mail.ru>
2020-01-13 23:14:09 +0300

    Add toolbar

--- app/src/main/java/ru/app/incredible/pets/ui/cats/CatsScreen.kt
+++ app/src/main/java/ru/app/incredible/pets/ui/cats/CatsScreen.kt
@@ -1,5 +1,6 @@
 package ru.app.incredible.pets.ui.cats
 
+import kotlinx.android.synthetic.main.toolbar.view.*
 import ru.app.incredible.pets.R
 import org.koin.android.ext.android.get
 import ru.app.incredible.pets.ui.common.BaseScreen
@@ -9,4 +10,10 @@ class CatsScreen : BaseScreen<CatsPm>() {
     override val screenLayout = R.layout.cats_screen
 
     override fun providePresentationModel(): CatsPm = get()
+
+    override fun onInitView() {
+        super.onInitView()
+
+        view?.toolbar?.setTitle(R.string.cats)
+    }
 }
--- app/src/main/java/ru/app/incredible/pets/ui/dogs/DogsScreen.kt
+++ app/src/main/java/ru/app/incredible/pets/ui/dogs/DogsScreen.kt
@@ -1,6 +1,6 @@
 package ru.app.incredible.pets.ui.dogs
 
-
+import kotlinx.android.synthetic.main.toolbar.view.*
 import ru.app.incredible.pets.R
 import org.koin.android.ext.android.get
 import ru.app.incredible.pets.ui.common.BaseScreen
@@ -10,4 +10,10 @@ class DogsScreen : BaseScreen<DogsPm>() {
     override val screenLayout = R.layout.dogs_screen
 
     override fun providePresentationModel(): DogsPm = get()
+
+    override fun onInitView() {
+        super.onInitView()
+
+        view?.toolbar?.setTitle(R.string.dogs)
+    }
 }
