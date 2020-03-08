3db8771dd9b7f46f72983184626dba916250c439
Alexander Rovnov <asrovnov@mail.ru>
2020-03-08 16:05:39 +0300

    Add base list adapter

---
+++ app/src/main/java/ru/app/incredible/pets/ui/common/adapter/BaseListAdapter.kt
@@ -0,0 +1,27 @@
+package ru.app.incredible.pets.ui.common.adapter
+
+import androidx.recyclerview.widget.ListAdapter
+import io.reactivex.functions.Consumer
+import ru.app.incredible.pets.domain.Identifiable
+
+abstract class BaseListAdapter<T : Identifiable, VH : BaseViewHolder<T>> :
+    ListAdapter<T, VH>(SimpleItemDiffCallback()),
+    Consumer<List<T>> {
+
+    override fun accept(data: List<T>) {
+        submitList(data)
+    }
+
+    init {
+        setHasStableIds(true)
+    }
+
+    override fun getItemId(position: Int): Long {
+        return getItem(position).id
+    }
+
+    final override fun onBindViewHolder(holder: VH, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+}
---
+++ app/src/main/java/ru/app/incredible/pets/ui/common/adapter/BaseViewHolder.kt
@@ -0,0 +1,12 @@
+package ru.app.incredible.pets.ui.common.adapter
+
+import android.content.res.Resources
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.extensions.LayoutContainer
+
+abstract class BaseViewHolder<in I>(override val containerView: View) :
+    RecyclerView.ViewHolder(containerView), LayoutContainer {
+    val resources: Resources get() = containerView.resources
+    abstract fun bind(item: I)
+}
